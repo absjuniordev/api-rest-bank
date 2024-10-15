73c6f56078c58522b6321d282c859ee0b168c8e9
Arnaldo Junior <abs.junnior@hotmail.com>
2024-10-15 20:58:29 -0300

    Initial commit

---
+++ src/test/java/com/absjuniordev/ApiRestSpringboot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.absjuniordev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSpringboot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
