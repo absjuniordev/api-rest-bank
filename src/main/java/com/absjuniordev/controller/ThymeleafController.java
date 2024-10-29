806efa4a98b7f1d5bc938017cbc3fd8f3566d959
Arnaldo Junior <abs.junnior@hotmail.com>
2024-10-29 17:54:25 -0300

    Application of the thymeleaf tool for frontend

---
+++ src/main/java/com/absjuniordev/controller/ThymeleafController.java
@@ -0,0 +1,46 @@
+package com.absjuniordev.controller;
+
+import com.absjuniordev.domain.model.User;
+import com.absjuniordev.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ThymeleafController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("nome", "Mundo");
+        return "index";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+        return "register"; // nome do template Thymeleaf
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute User user) {
+        userService.create(user);
+        return "redirect:/success";
+    }
+
+    @GetMapping("/success")
+    public String showSuccessPage() {
+        return "success";
+    }
+
+    @GetMapping("/list")
+    public String listUsers(Model model) {
+        model.addAttribute("users", userService.findAll());
+        return "users";
+    }
+
+}
